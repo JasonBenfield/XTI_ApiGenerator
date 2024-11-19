670d1670a60bb139f86b4a75e3ccc92d91288de8
Jason Benfield <jbenfield108@gmail.com>
2024-11-18 20:26:29 -0500

    upgrade to dotnet 9

--- Output/FakeWebApp/Scripts/Fake/Api/AddEmployeeForm.ts
+++ Output/FakeWebApp/Scripts/Fake/Api/AddEmployeeForm.ts
@@ -31,9 +31,9 @@ export class AddEmployeeForm extends BaseForm {
 		this.HireDate.setItems(
 			'',
 			[
-				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(4), 15), 'Yesterday'),
-				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(4), 16), 'Today'),
-				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(4), 17), 'Tomorrow')
+				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(11), 12), 'Yesterday'),
+				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(11), 13), 'Today'),
+				new DropDownFieldItem(new xti.DateOnly(2024, xti.Month.fromValue(11), 14), 'Tomorrow')
 			]
 		);
 		this.IsTemp.setCaption('Is Temp');
